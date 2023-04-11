3450fef6b6f00717c5d91cbfbed3e7dea3e6108e
Kostya <konstantin_kri@protonmail.com>
2023-04-11 22:28:38 +0300

    - Added default config name, and custom type hints.  - Added missing data for CLI generation.  - Added a method for parsing the args after generation.  - Added a method for parsing the config file.  - Replaced magic values with user args.  - Saveing the resulting file.

--- scripts/render_template.py
+++ scripts/render_template.py
@@ -1,7 +1,15 @@
 import argparse
 import json
+import os
+from collections.abc import Iterable, Mapping
+from typing import Union, Optional
 
 import jinja2
+
+
+DEFAULT_CONFIG_KEY = "default"
+Config = Mapping[str, str]
+MetaConfig = Mapping[str, Config]
 
 
 def get_args():
@@ -9,19 +17,58 @@ def get_args():
     :return: argument parser
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--work-dir", help="",
+    parser.add_argument("--work-dir", help="The path to the work directory. "\
+                                           "If provided, all other path arguments should be given relative to this.",
                         type=str, default=None)
-    parser.add_argument("--tmpl-name", help="",
-                        type=str, default=None)
-    parser.add_argument("--cnfg-name", help="",
-                        type=str, default=None)
-    parser.add_argument("--cnfg-key", help="",
-                        type=str, default=None)
+    parser.add_argument("--tmpl-path", help="A path to the template file.",
+                        type=str, default=None, required=True)
+    parser.add_argument("--cnfg-path", help="A path to the config file. "
+                                            "It should be a json format, with a dict of dicts. "
+                                            "Each dict in the main level represents a different config. "
+                                            "The structure can also be put in the 2nd place of a 2 item list, "
+                                            "with the 1st item being the name of the selected config.",
+                        type=str, default=None, required=True)
+    parser.add_argument("--cnfg-key", help="The chosen config to be applied to the template from the config file.",
+                        type=str, default=DEFAULT_CONFIG_KEY)
 
-    parser.add_argument("--dst-name", help="",
-                        type=str, default=None)
+    parser.add_argument("--dest-path", help="A path to the output location of the file.",
+                        type=str, default=None, required=True)
 
     return parser
+
+
+def handle_args(args: Mapping[str,]) -> tuple[Mapping[str,], str]:
+    """
+    :param args: The raw arguments passed by the user.
+    """
+    if args.work_dir is not None:
+        args.tmpl_path = os.path.join(args.tmpl_path, args.work_dir)
+        args.cnfg_path = os.path.join(args.cnfg_path, args.work_dir)
+        args.dest_path = os.path.join(args.dest_path, args.work_dir)
+    config_name : str = args.cnfg_key
+    return args, config_name
+
+
+def handle_configs(args: Mapping[str,], 
+                   user_configs: Union[MetaConfig, tuple[str, MetaConfig]], 
+                   config_name: Optional[str] = None) -> Config:
+    """
+    :param args: Arguments passed tot he program.
+    :param user_configs: A collection of configurations loaded from a config file.
+    :param config_name: The config to be chosen from the config file.
+    """
+    if isinstance(user_configs, Mapping):
+        user_configs: MetaConfig
+    elif isinstance(user_configs, Iterable):
+        # assert (len(user_configs) >= 2) and (len(user_configs) <= 3)
+        assert len(user_configs) == 2
+        config_name: str = config_name if config_name is not None else user_configs[0]
+        user_configs: MetaConfig = user_configs[-1]
+        return handle_configs(args, user_configs, config_name)
+    else:
+        raise TypeError("The config file does not match any of the expected formats.")
+    config_name = config_name if config_name is not None else DEFAULT_CONFIG_KEY
+    return user_configs[config_name]
 
 
 def render_template(args=None):
@@ -30,14 +77,21 @@ def render_template(args=None):
     """
     if args is None:
         args = get_args().parse_args()
+    args, config_name = handle_args(args)
+
     tmplLoader = jinja2.FileSystemLoader(searchpath="./")
     tmplEnv = jinja2.Environment(loader=tmplLoader)
 
-    tmpl = tmplEnv().get_template("README.md.tpl")  # TODO: replace magic word with args
-    user_args = dict()  # TODO: load user args using args
-    outp = tmpl.render(**user_args)
-
-    # TODO: save outp as a file
+    tmpl = tmplEnv().get_template(args.tmpl_path)
+    
+    with open(args.cnfg_path, 'r') as file:
+        user_configs = json.load(file)
+        user_configs = handle_configs(args, user_configs, config_name)
+    
+    
+    outp = tmpl.render(**user_configs)
+    with open(args.dest_path, 'w') as file:
+        file.writelines(outp)
 
 
 if __name__ == "__main__":
